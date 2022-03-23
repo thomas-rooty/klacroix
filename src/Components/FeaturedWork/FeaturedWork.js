e53844f056673717992d3a7a87eac26d95bc27f4
thomas-rooty <essainono1@gmail.com>
2022-03-23 11:18:19 +0100

    added samples

--- src/Components/FeaturedWork/FeaturedWork.js
+++ src/Components/FeaturedWork/FeaturedWork.js
@@ -3,15 +3,27 @@ import "./FeaturedWork.css";
 import {Parallax, Background} from 'react-parallax';
 import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
 import FeaturedWorkBg from './Assets/Images/FeaturedWorkBg.jpg';
+import FirstVideo from './Assets/Images/Projects/1.jpg';
+import SecondVideo from './Assets/Images/Projects/2.jpg';
 
 const FeaturedWork = () => {
     return (
         <section id='featuredwork'>
             <Parallax bgImage={FeaturedWorkBg} strength={500}>
-                <div style={{height: 900}}>
+                <div style={{height: 800}}>
                     <div className="fw-content">
                         <div className="fw-header">
                             <h2 className='fw-title glowingText'>Featured Content</h2>
+                        </div>
+                        <div className="fw-showcase">
+                            <div className="fw-oneShowcase">
+                                <img src={FirstVideo} alt=""/>
+                                <h2 className='fw-title glowingText'>Shot Edition #2</h2>
+                            </div>
+                            <div className="fw-oneShowcase">
+                                <img src={SecondVideo} alt=""/>
+                                <h2 className='fw-title glowingText'>One afternoon to shoot in Paris</h2>
+                            </div>
                         </div>
                     </div>
                 </div>
