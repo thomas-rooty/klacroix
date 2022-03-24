b645a2847556d1c90f771070270ce4f7adfdb38b
thomas-rooty <essainono1@gmail.com>
2022-03-24 14:09:14 +0100

    added loading state

--- src/App.js
+++ src/App.js
@@ -10,14 +10,37 @@ import Presentation from "./Components/Presentation/Presentation";
 import Footer from "./Components/Footer/Footer";
 
 const App = () => {
+    const [isSiteReady, setIsSiteReady] = React.useState(false);
+
+    // This will run one time after the component mounts
+    React.useEffect(() => {
+        const onPageLoad = () => {
+            setIsSiteReady(true);
+        };
+
+        // Check if the page has already loaded
+        if (document.readyState === "complete") {
+            onPageLoad();
+        } else {
+            window.addEventListener("load", onPageLoad);
+            // Remove the event listener when component unmounts
+            return () => window.removeEventListener("load", onPageLoad);
+        }
+    }, []);
     return (
         <div className="App">
-            <Hero/>
-            <Navbar/>
-            <FeaturedWork/>
-            <Brands/>
-            <Presentation/>
-            <Footer/>
+            {isSiteReady ? (
+                <>
+                    <Hero/>
+                    <Navbar/>
+                    <FeaturedWork/>
+                    <Brands/>
+                    <Presentation/>
+                    <Footer/>
+                </>
+            ) : (
+                <div>Loading...</div>
+            )}
         </div>
     );
 }
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Hero.css';
 import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
 import HeroVideo from 'url:./Assets/HeroVideo.mp4';
+import HeroVideoCompressed from 'url:./Assets/HeroVideoCompressed.mp4';
 import ArrowRight from './Icons/arrow-right.svg';
 
 const Hero = () => {
-    // Fixed video in the center of the screen (hero.mp4)
     return (
         <section id="home">
             <div className="hero">
                 <video autoPlay loop muted>
-                    <source src={HeroVideo} type="video/mp4"/>
+                    <source src={HeroVideoCompressed} type="video/mp4"/>
                 </video>
                 <div className="hero-text-box">
                     <h1 className='glowingText'>LAC X FILMS</h1>
