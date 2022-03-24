23ad2d0e9e56cc31b075d8382600f6aa0b3801b3
thomas-rooty <essainono1@gmail.com>
2022-03-24 14:57:46 +0100

    forcing html rendering

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,30 +7,16 @@ import HeroVideoMobile from 'url:./Assets/HeroVideoCompressed.gif';
 import ArrowRight from './Icons/arrow-right.svg';
 
 const Hero = () => {
-    const [isMobile, setIsMobile] = React.useState(false);
-    const getWindowWidth = () => {
-        return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    };
-
-    React.useEffect(() => {
-        let windowWidth = getWindowWidth();
-        if (windowWidth < 1100) {
-            setIsMobile(true);
-        } else {
-            setIsMobile(false);
-        }
-    }, []);
-
     return (
         <section id="home">
             <div className="hero">
-                {!isMobile ?
-                    <video id="hero-video" autoPlay loop muted>
-                        <source src={HeroVideoCompressed} type="video/mp4"/>
-                    </video>
-                    :
-                    <img className="videoGif" id="hero-video" src={HeroVideoMobile} alt=""/>
-                }
+                <div dangerouslySetInnerHTML={{
+                    __html: `
+                <video id="hero-video" loop muted playsinline autoplay>
+                    <source src="${HeroVideoCompressed}" type="video/mp4" />
+                </video>
+            `
+                }}/>
                 <div className="hero-text-box">
                     <h1 className='glowingText'>LAC X FILMS</h1>
                     <h2 className='glowingText'>KEVIN LACROIX</h2>
