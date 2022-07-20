52c1a764e2fc71e8398bf3971644ec754311d8c9
thomas-rooty <tc.caron91@gmail.com>
2022-07-20 14:05:23 +0200

    working translations

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,51 +2,35 @@ import React from 'react';
 import './Hero.css';
 import AnchorLink from "react-anchor-link-smooth-scroll/lib/anchor-link";
 import ArrowRight from './Icons/arrow-right.svg';
-import {storage} from '../../Services/Firebase';
-import {getStorage, ref, getDownloadURL} from "firebase/storage";
 import HeroVideo from 'url:./HeroVideo.mp4';
+import {useTranslation} from "react-i18next";
 
 const Hero = ({value: {setIsSiteReady}}) => {
-    const [video, setVideo] = React.useState(null);
-    React.useEffect(() => {
-        const storage = getStorage();
+	const {t} = useTranslation();
 
-        const storageRef = ref(storage);
-        const videosRef = ref(storageRef, 'videos');
-
-        const fileName = 'shot in paris 4k DCI.mp4';
-        const videoRef = ref(videosRef, fileName);
-
-        // Get the download URL
-        getDownloadURL(videoRef).then(url => {
-            setVideo(url);
-            setIsSiteReady(true);
-        });
-    }, []);
-
-
-    return (
-        <section id="home">
-            <div className="hero">
-                <div dangerouslySetInnerHTML={{
-                    __html: `
+	return (
+		<section id="home">
+			<div className="hero">
+				<div dangerouslySetInnerHTML={{
+					__html: `
                 <video id="hero-video" loop muted playsinline autoplay>
                     <source src="${HeroVideo}" type="video/mp4" />
                 </video>
             `
-                }}/>
-                <div className="hero-text-box">
-                    <h1 className='glowingText'>LAC X FILMS</h1>
-                    <h2 className='glowingText'>KEVIN LACROIX</h2>
-                    <AnchorLink href='#featuredwork'>
-                        <button className='btn-banner glowingText' type="button">Featured Work
-                            <img src={ArrowRight} alt="Arrow Right"/>
-                        </button>
-                    </AnchorLink><br/>
-                </div>
-            </div>
-        </section>
-    );
+				}}/>
+				<div className="hero-text-box">
+					<h1 className='glowingText'>LAC X FILMS</h1>
+					<h2 className='glowingText'>KEVIN LACROIX</h2>
+					<AnchorLink href='#featuredwork'>
+						<button className='btn-banner glowingText' type="button">
+							{t("hello_welcome_to_react")}
+							<img src={ArrowRight} alt="Arrow Right"/>
+						</button>
+					</AnchorLink><br/>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Hero;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,58 +1,74 @@
-import React from 'react';
-import RideCeption from '../../Assets/rideception_logo.png';
-import LGLogoBlackBg from './Assets/Icons/LFLogoBlackBg.png';
+import React, {useEffect} from 'react';
 import Signature from './Assets/Icons/Signature.png';
 import Instagram from './Assets/Icons/instagram.png';
 import Linkedin from './Assets/Icons/linkedin.png';
 import Tiktok from './Assets/Icons/tiktok.png';
 import Youtube from './Assets/Icons/youtube.png';
 import './Navbar.css';
+import {useTranslation} from "react-i18next";
+import i18next from "i18next";
+
+const languageMap = {
+	en: {label: "English", dir: "ltr", active: true},
+	fr: {label: "Français", dir: "ltr", active: false}
+};
 
 // Fixed navbar that hide on scroll down
 const Navbar = () => {
-    // Add event listener window scroll
-    window.addEventListener('scroll', () => {
-        let currentScrollPos = window.pageYOffset;
-        if (currentScrollPos < 225) {
-            document.getElementById("navbar").style.top = "0";
-        } else {
-            document.getElementById("navbar").style.top = "-80px";
-        }
-    });
+	const selected = localStorage.getItem("i18nextLng") || "en";
+	const {t} = useTranslation();
 
-    // Change active a item on click
-    const changeActive = (e) => {
-        let active = document.getElementsByClassName("active");
-        active[0].className = active[0].className.replace("active", "");
-        e.target.className += "active";
-    }
+	// Add event listener window scroll
+	window.addEventListener('scroll', () => {
+		let currentScrollPos = window.pageYOffset;
+		if (currentScrollPos < 225) {
+			document.getElementById("navbar").style.top = "0";
+		} else {
+			document.getElementById("navbar").style.top = "-80px";
+		}
+	});
 
-    return (
-        <div id="navbar">
-            <div className='navbar-left'>
-                <img className='logoNavbar' src={Signature} alt="Ride ception logo"/>
-            </div>
-            <div className="navbar-center">
-                <a href="#home" className="active" onClick={changeActive}>HOME</a>
-                <a href="#about" onClick={changeActive}>ABOUT</a>
-                <a href="#contact" onClick={changeActive}>CONTACT</a>
-            </div>
-            <div className='navbar-right'>
-                <a href="https://www.youtube.com/channel/UCOU98_rG1FFUKVPWta1Z7eQ" target="_blank" rel="noopener noreferrer">
-                    <img className='socialmedia' src={Youtube} alt="Youtube"/>
-                </a>
-                <a href="https://www.tiktok.com/@lacroix__kevin" target="_blank" rel="noopener noreferrer">
-                    <img className='socialmedia' src={Tiktok} alt="Tiktok"/>
-                </a>
-                <a href="https://www.instagram.com/kevin__lacroix/" target="_blank" rel="noopener noreferrer">
-                    <img className='socialmedia' src={Instagram} alt="Instagram"/>
-                </a>
-                <a href="https://www.linkedin.com/in/kevin-lacroix-b5141a236/" target="_blank" rel="noopener noreferrer">
-                    <img className='socialmedia' src={Linkedin} alt="Linkedin"/>
-                </a>
-            </div>
-        </div>
-    )
+	// Change active a item on click
+	const changeActive = (e) => {
+		let active = document.getElementsByClassName("active");
+		active[0].className = active[0].className.replace("active", "");
+		e.target.className += "active";
+	}
+
+	return (
+		<div id="navbar">
+			<div className='navbar-left'>
+				<img className='logoNavbar' src={Signature} alt="Ride ception logo"/>
+			</div>
+			<div className="navbar-center">
+				<a href="#home" className="active" onClick={changeActive}>HOME</a>
+				<a href="#about" onClick={changeActive}>ABOUT</a>
+				<a href="#contact" onClick={changeActive}>CONTACT</a>
+			</div>
+			<div className="navbar-language">
+				<a href="#" onClick={() => i18next.changeLanguage("en")}>
+					{languageMap["en"].label}
+				</a>
+				<a href="#" onClick={() => i18next.changeLanguage("fr")}>
+					{languageMap["fr"].label}
+				</a>
+			</div>
+			<div className='navbar-right'>
+				<a href="https://www.youtube.com/channel/UCOU98_rG1FFUKVPWta1Z7eQ" target="_blank" rel="noopener noreferrer">
+					<img className='socialmedia' src={Youtube} alt="Youtube"/>
+				</a>
+				<a href="https://www.tiktok.com/@lacroix__kevin" target="_blank" rel="noopener noreferrer">
+					<img className='socialmedia' src={Tiktok} alt="Tiktok"/>
+				</a>
+				<a href="https://www.instagram.com/kevin__lacroix/" target="_blank" rel="noopener noreferrer">
+					<img className='socialmedia' src={Instagram} alt="Instagram"/>
+				</a>
+				<a href="https://www.linkedin.com/in/kevin-lacroix-b5141a236/" target="_blank" rel="noopener noreferrer">
+					<img className='socialmedia' src={Linkedin} alt="Linkedin"/>
+				</a>
+			</div>
+		</div>
+	)
 }
 
 export default Navbar;
