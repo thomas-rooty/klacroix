db55990e4fab715e2f0f8fb0826d2f604912956e
thomas-rooty <tc.caron91@gmail.com>
2022-07-20 14:42:39 +0200

    finished translation

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Modal from "react-modal";
 import ContactForm from '../ContactForm/ContactForm';
 import "./Contact.css";
+import {useTranslation} from "react-i18next";
 
 // noinspection JSUnresolvedFunction
 Modal.setAppElement('#root');
 
 const Contact = ({setIsOpen, modalIsOpen}) => {
+	const {t} = useTranslation();
 
 	const closeModal = (e) => {
 		e.stopPropagation();
@@ -23,7 +25,7 @@ const Contact = ({setIsOpen, modalIsOpen}) => {
 		>
 			<div className="contact-modal-content">
 				<div className="contact-modal-header">
-					<h2 className="contact-modal-title">Contact me</h2>
+					<h2 className="contact-modal-title">{t("contact_me")}</h2>
 					<button className="closeBtn" onClick={closeModal}>X</button>
 				</div>
 				<div className="contact-modal-body">
--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,42 +1,44 @@
 import React from 'react';
 import "./ContactForm.css";
 import {useForm, ValidationError} from "@formspree/react";
+import {useTranslation} from "react-i18next";
 
 const ContactForm = () => {
+	const {t} = useTranslation();
 	const [state, handleSubmit] = useForm("mvolnole");
 	if (state.succeeded) {
-		return <p>Thanks for joining me!</p>;
+		return <p>{t("thanks_contact")}</p>;
 	}
 
 	return (
 		<form className="form-container" onSubmit={handleSubmit}>
 			<div className="form-group">
 				<div className="form-row">
-					<label htmlFor="name">Name</label>
-					<input type="text" name="name" id="name" placeholder="How should I name you?"/>
+					<label htmlFor="name">{t("name")}</label>
+					<input type="text" name="name" id="name" placeholder={t("placeholder_name")}/>
 					<ValidationError field="name" errors={state.errors}/>
 				</div>
 
 				<div className="form-row">
 					<label htmlFor="email">Email</label>
-					<input type="email" name="email" id="email" placeholder="How can I recontact you?"/>
+					<input type="email" name="email" id="email" placeholder={t("placeholder_mail")}/>
 					<ValidationError field="email" errors={state.errors}/>
 				</div>
 
 				<div className="form-row">
-					<label htmlFor="subject">Subject</label>
-					<input type="text" name="subject" id="subject" placeholder="Why are you contacting me?"/>
+					<label htmlFor="subject">{t("subject")}</label>
+					<input type="text" name="subject" id="subject" placeholder={t("placeholder_subject")}/>
 					<ValidationError field="subject" errors={state.errors}/>
 				</div>
 
 				<div className="form-row">
 					<label htmlFor="message">Message</label>
-					<textarea name="message" id="message" cols="30" rows="10" placeholder="Tell me everything!"></textarea>
+					<textarea name="message" id="message" cols="30" rows="10" placeholder={t("placeholder_message")}></textarea>
 					<ValidationError field="message" errors={state.errors}/>
 				</div>
 			</div>
 
-			<button className="submitBtn" type="submit">Submit</button>
+			<button className="submitBtn" type="submit">{t("submit")}</button>
 		</form>
 	);
 }
